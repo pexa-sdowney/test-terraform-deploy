05cb126b47029d488be5491d7fcb36879e1486c1
Sam Downey <sam.downey@pexa.com.au>
2021-07-21 15:26:37 +1000

    network mode

--- terraform/task_def.tf
+++ terraform/task_def.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_task_definition" "yt_archiver" {
   family                   = "yt-archiver"
   task_role_arn            = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
   execution_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
-  #network_mode             = "awsvpc"
+  network_mode             = "bridge" #awsvpc
   cpu                      = "256"
   memory                   = "512"
   requires_compatibilities = ["FARGATE"]
