a883dc08b6afd8adf0169b2720855ee5ddaa97ea
Sam Downey <sam.downey@pexa.com.au>
2021-07-21 16:44:17 +1000

    ad lgos

--- terraform/task_def.tf
+++ terraform/task_def.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_task_definition" "yt_archiver" {
   family                   = "yt-archiver"
   task_role_arn            = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
   execution_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
-  network_mode             = "awsvpc"
+  network_mode             = "bridge" #awsvpc
   cpu                      = "256"
   memory                   = "512"
   requires_compatibilities = ["FARGATE"]
@@ -11,15 +11,16 @@ resource "aws_ecs_task_definition" "yt_archiver" {
   [
     {
       "image": "public.ecr.aws/g4k5i3z6/yt-archiver",
-      "name": "yt-archiver",
+      "name": "yt-archiver"
       "logConfiguration": {
-                "logDriver": "awslogs",
-                "options": {
-                    "awslogs-group": "fargate",
-                    "awslogs-region": "ap-southeast-2",
-                    "awslogs-stream-prefix": "ecs-fargate-yt-archiver",
-                    "awslogs-create-group": "true"
+          "logDriver": "awslogs",
+          "options": {
+              "awslogs-group": "fargate",
+              "awslogs-region": "ap-southeast-2",
+              "awslogs-stream-prefix": "ecs-fargate-yt-archiver",
+              "awslogs-create-group": "true"
                 }
+    }
     }
   ]
   EOF
