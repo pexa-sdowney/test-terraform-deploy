4344a4160e09d9f257fb8891592ab125e838e272
Sam Downey <sam.downey@pexa.com.au>
2021-07-23 15:42:53 +1000

    update task def

--- terraform/task_def.tf
+++ terraform/task_def.tf
@@ -8,46 +8,28 @@ resource "aws_ecs_task_definition" "yt_archiver" {
   requires_compatibilities = ["FARGATE"]
 
   container_definitions = <<EOF
-  [
-    {
-      "image": "public.ecr.aws/g4k5i3z6/yt-archiver",
-      "name": "yt-archiver",
-      "logConfiguration": {
-          "logDriver": "awslogs",
-          "options": {
-              "awslogs-group": "fargate",
-              "awslogs-region": "ap-southeast-2",
-              "awslogs-stream-prefix": "ecs-fargate-yt-archiver",
-              "awslogs-create-group": "true"
-                }
-    }
-    }
-  ]
+  [{
+  	"image": "public.ecr.aws/g4k5i3z6/yt-archiver",
+  	"name": "yt-archiver",
+  	"logConfiguration": {
+  		"logDriver": "awslogs",
+  		"options": {
+  			"awslogs-group": "fargate",
+  			"awslogs-region": "ap-southeast-2",
+  			"awslogs-stream-prefix": "ecs-fargate-yt-archiver",
+  			"awslogs-create-group": "true"
+  		}
+  	},
+  	"volumes": [{
+  		"name": "efs",
+  		"efsVolumeConfiguration": {
+  			"fileSystemId": "fs-88181eb0",
+  			"rootDirectory": "/downloads",
+  		}
+  	}]
+
+  }]
   EOF
-}
-
-resource "aws_ecr_lifecycle_policy" "foopolicy" {
-  repository = "yt-archiver"
-
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Expire untagged images older than 1 day",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "sinceImagePushed",
-                "countUnit": "days",
-                "countNumber": 1
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
+  }
 
 data "aws_caller_identity" "current" {}
