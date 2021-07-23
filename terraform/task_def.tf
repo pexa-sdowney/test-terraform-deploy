641516d75de7664142435c4e29031a1a427a72f4
Sam Downey <sam.downey@pexa.com.au>
2021-07-23 15:25:28 +1000

    add lifecycle policy

--- terraform/task_def.tf
+++ terraform/task_def.tf
@@ -26,4 +26,28 @@ resource "aws_ecs_task_definition" "yt_archiver" {
   EOF
 }
 
+resource "aws_ecr_lifecycle_policy" "foopolicy" {
+  repository = "yt-archiver"
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire untagged images older than 1 day",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
 data "aws_caller_identity" "current" {}
